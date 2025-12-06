6fd9468ed6f4d7a5ffc4664aec33745a761676b8
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-12-06 20:36:14 +0100

    Add ImPlot version to about window

--- platforms/shared/desktop/gui_popups.cpp
+++ platforms/shared/desktop/gui_popups.cpp
@@ -238,6 +238,7 @@ void gui_popup_modal_about(void)
                 add_build_info("SDL %d.%d.%d (link)\n", application_sdl_link_version.major, application_sdl_link_version.minor, application_sdl_link_version.patch);
                 add_build_info("OpenGL %s\n", renderer_opengl_version);
                 add_build_info("Dear ImGui %s (%d)\n", IMGUI_VERSION, IMGUI_VERSION_NUM);
+                add_build_info("ImPlot %s (%d)\n", IMPLOT_VERSION, IMPLOT_VERSION_NUM);
 
                 #if defined(DEBUG)
                 add_build_info("define: DEBUG\n");
