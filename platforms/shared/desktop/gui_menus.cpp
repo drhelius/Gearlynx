8c1aeee674249d8fe3f8ff708817a036a7197cc3
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-02-01 11:41:48 +0100

    Add fullscreen modes. #12

--- platforms/shared/desktop/gui_menus.cpp
+++ platforms/shared/desktop/gui_menus.cpp
@@ -499,6 +499,25 @@ static void menu_video(void)
             application_trigger_fullscreen(config_emulator.fullscreen);
         }
 
+#if !defined(__APPLE__)
+        if (ImGui::BeginMenu("Fullscreen Mode"))
+        {
+            ImGui::PushItemWidth(130.0f);
+            if (ImGui::Combo("##fullscreen_mode", &config_emulator.fullscreen_mode, "Exclusive\0Fake Exclusive\0Borderless\0\0"))
+            {
+                if (config_emulator.fullscreen)
+                {
+                    application_trigger_fullscreen(false);
+                    application_trigger_fullscreen(true);
+                }
+            }
+            ImGui::PopItemWidth();
+            ImGui::EndMenu();
+        }
+#endif
+
+        ImGui::Separator();
+
         ImGui::MenuItem("Always Show Menu", config_hotkeys[config_HotkeyIndex_ShowMainMenu].str, &config_emulator.always_show_menu);
         if (ImGui::IsItemHovered())
         {
