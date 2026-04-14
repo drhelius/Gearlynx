559eaf400a9861bbcd710c875f2a60083feb53f7
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-04-14 13:34:39 +0200

    Fix unused variable warning in libretro builds

--- src/gearlynx_core.cpp
+++ src/gearlynx_core.cpp
@@ -558,7 +558,9 @@ bool GearlynxCore::LoadState(std::istream& stream)
     }
 
     GLYNX_SaveState_Header_Libretro header;
+#if !defined(__LIBRETRO__)
     bool is_desktop_savestate = false;
+#endif
 
     stream.seekg(0, ios::end);
     size_t size = static_cast<size_t>(stream.tellg());
@@ -574,7 +576,9 @@ bool GearlynxCore::LoadState(std::istream& stream)
         {
             header.magic = desktop_header.magic;
             header.version = desktop_header.version;
+#if !defined(__LIBRETRO__)
             is_desktop_savestate = true;
+#endif
             Log("Loading desktop save state");
         }
     }
