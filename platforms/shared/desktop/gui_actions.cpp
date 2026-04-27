90d31015928915e23c50bd38a2029a16bdef2e2b
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-04-27 22:23:12 +0200

    Fix warnings

--- platforms/shared/desktop/gui_actions.cpp
+++ platforms/shared/desktop/gui_actions.cpp
@@ -123,10 +123,9 @@ void gui_action_save_screenshot(const char* path)
     time_t now = time(0);
     tm* ltm = localtime(&now);
 
-    char date_time[32];
-    snprintf(date_time, sizeof(date_time), "%04d-%02d-%02d %02d%02d%02d",
-             1900 + ltm->tm_year, 1 + ltm->tm_mon, ltm->tm_mday,
-             ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
+    char date_time[32] = {};
+    if (ltm != NULL)
+        strftime(date_time, sizeof(date_time), "%Y-%m-%d %H%M%S", ltm);
 
     string file_path;
 
