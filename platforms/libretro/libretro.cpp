fc8d49d8ba1f53c57c9aef5c14e9e02c7a6d051f
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-05-29 19:59:27 +0200

    [libretro] Apply input before frame emulation

--- platforms/libretro/libretro.cpp
+++ platforms/libretro/libretro.cpp
@@ -62,7 +62,6 @@ static float aspect_ratio = 0.0f;
 static float current_fps = 60.0f;
 
 static bool allow_up_down = false;
-static bool input_updated = false;
 static bool categories_supported = false;
 
 static bool libretro_supports_bitmasks;
@@ -299,13 +298,10 @@ void retro_run(void)
     if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &core_options_updated) && core_options_updated)
         check_variables();
 
+    update_input();
+
     audio_sample_count = 0;
     core->RunToVBlank(frame_buffer, audio_buf, &audio_sample_count);
-
-    if (!input_updated)
-        update_input();
-
-    input_updated = false;
 
     core->GetRuntimeInfo(runtime_info);
 
@@ -481,11 +477,6 @@ static void set_controller_info(void)
 
 static void update_input(void)
 {
-    if (input_updated)
-        return;
-
-    input_updated = true;
-
     int16_t joypad_bits[MAX_PADS];
 
     input_poll_cb();
