38e8e500386616e274dabefe6a7ff411fa76719b
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-05-31 11:33:08 +0200

    Desktop light theme

--- platforms/shared/desktop/gui.h
+++ platforms/shared/desktop/gui.h
@@ -123,6 +123,7 @@ EXTERN void gui_render(void);
 EXTERN void gui_shortcut(gui_ShortCutEvent event);
 EXTERN void gui_load_rom(const char* path);
 EXTERN void gui_load_bios(const char* path);
+EXTERN void gui_set_style(void);
 EXTERN void gui_set_status_message(const char* message, Uint64 milliseconds);
 EXTERN void gui_set_error_message(const char* message);
 
